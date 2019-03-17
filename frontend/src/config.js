924a48f836af9427d76b9c2eff7e3352cc92faaf
LaQuay <marcvilagomez@gmail.com>
2019-03-17 16:50:51 +0100

    Improve frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   services: {
     api_out_of_the_box: {
-      url: ''
+      url: 'http://192.168.1.64:5000'
     }
   }
 }
--- frontend/src/views/entry.js
+++ frontend/src/views/entry.js
@@ -1,14 +1,39 @@
 import React from 'react'
 
+import EntryAPI from '../api/entryAPI.js'
+import Entry from '../models/entry.js'
+
 export class EntryTab extends React.Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      entriesRows: []
+    }
+  }
+
+  componentDidMount() {
+    EntryAPI.getAllEntries(false, response => {
+      var results = response.message.map(entry => {
+        return new Entry(entry.id, entry.value, entry.date)
+      })
+      this.setState({ entriesRows: results })
+    })
   }
 
   render() {
     return (
       <div className="container-fluid">
-        HOLA
+        {this.state.entriesRows.map(entry => {
+          return (
+            <div key={entry.id}>
+              <dt>{entry.id}</dt>
+              <dd>{entry.value}</dd>
+              <dd>{entry.date}</dd>
+              <hr />
+            </div>
+          )
+        })}
       </div>
     )
   }
