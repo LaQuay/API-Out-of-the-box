eead8fb79eddf7f03d09332021d2b8b5961ee95f
LaQuay <marcvilagomez@gmail.com>
2019-04-23 16:00:55 +0200

    update frontend and android URLS

--- android/app/src/main/java/com/laquay/apioutofthebox/utils/APIUtils.java
+++ android/app/src/main/java/com/laquay/apioutofthebox/utils/APIUtils.java
@@ -1,5 +1,5 @@
 package com.laquay.apioutofthebox.utils;
 
 public class APIUtils {
-    public static final String ENTRY_BASE_URL = "http://192.168.1.64:5000/entries/";
+    public static final String ENTRY_BASE_URL = "http://10.0.2.2:5000"; //Add here your backend URL
 }
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 const config = {
   services: {
     api_out_of_the_box: {
-      url: 'http://192.168.1.64:5000' //Add here your backend URL
+      url: 'http://localhost:5000' //Add here your backend URL
     }
   }
 }
